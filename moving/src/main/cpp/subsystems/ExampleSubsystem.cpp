d9a24121cdcda6d018f4a871e34036fd2e9113c6
NoodlesK <65808923+Noodle4219005@users.noreply.github.com>
2021-11-21 16:46:09 +0800

    main

--- moving/src/main/cpp/subsystems/ExampleSubsystem.cpp
+++ moving/src/main/cpp/subsystems/ExampleSubsystem.cpp
@@ -2,18 +2,18 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "subsystems/ExampleSubsystem.h"
+#include "..\\..\\include\\subsystems\\ExampleSubsystem.h"
+
 #include <VictorSPX.h>
 
 ExampleSubsystem::ExampleSubsystem() {
-  // Implementation of subsystem constructor goes here.
-
+    // Implementation of subsystem constructor goes here.
 }
 
 void ExampleSubsystem::Periodic() {
-  // Implementation of subsystem periodic method goes here.
+    // Implementation of subsystem periodic method goes here.
 }
 
 void ExampleSubsystem::SimulationPeriodic() {
-  // Implementation of subsystem simulation periodic method goes here.
+    // Implementation of subsystem simulation periodic method goes here.
 }
