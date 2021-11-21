c579a89b397b49179b325c8f96a55d669ea10057
Howard_yu <howard60211@gmail.com>
2021-11-21 16:50:44 +0800

    main

--- moving/src/main/cpp/subsystems/ExampleSubsystem.cpp
+++ moving/src/main/cpp/subsystems/ExampleSubsystem.cpp
@@ -3,17 +3,22 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/ExampleSubsystem.h"
-#include <VictorSPX.h>
+#include "subsystems/VictorSPX.h"
+
+using namespace ctre::phoenix::motorcontrol::can;
 
 ExampleSubsystem::ExampleSubsystem() {
-  // Implementation of subsystem constructor goes here.
-
+	// Implementation of subsystem constructor goes here.
+	VictorSPX left1 = VictorSPX{0};
+	VictorSPX left2 = VictorSPX{1};
+	VictorSPX right1 = VictorSPX{2};
+	VictorSPX right2 = VictorSPX{3};
 }
 
 void ExampleSubsystem::Periodic() {
-  // Implementation of subsystem periodic method goes here.
+	// Implementation of subsystem periodic method goes here.
 }
 
 void ExampleSubsystem::SimulationPeriodic() {
-  // Implementation of subsystem simulation periodic method goes here.
+	// Implementation of subsystem simulation periodic method goes here.
 }
